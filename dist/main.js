cfe8477930223c7f6c094cb672d069c8f7409b42
Scott Sauber <stsauber@gmail.com>
2023-09-27 23:17:44 -0500

    working

--- dist/main.js
+++ dist/main.js
@@ -5,11 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const zod_1 = require("zod");
 const app = (0, express_1.default)();
-const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
 app.use(express_1.default.json());
-app.get("/", (req, res) => {
-    res.send({ firstName: "Blah" });
+app.use(body_parser_1.default.urlencoded());
+const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
+const schema = zod_1.z.object({
+    firstName: zod_1.z.string().min(2),
+    lastName: zod_1.z.string().optional(),
+});
+app.post("/customer", (req, res) => {
+    const parsedData = schema.safeParse(req.body);
+    if (!parsedData.success) {
+        return res.status(400).send(parsedData.error.issues);
+    }
+    res.status(200).send();
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- main.ts
+++ main.ts
@@ -1,12 +1,26 @@
 import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+import { z } from "zod";
 
 const app: Express = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded());
+
 const port = process.env.PORT ?? 3000;
 
-app.use(express.json());
+const schema = z.object({
+  firstName: z.string().min(2),
+  lastName: z.string().optional(),
+});
 
-app.get("/", (req: Request, res: Response) => {
-  res.send({ firstName: "Blah" });
+app.post("/customer", (req: Request, res: Response) => {
+  const parsedData = schema.safeParse(req.body);
+
+  if (!parsedData.success) {
+    return res.status(400).send(parsedData.error.issues);
+  }
+
+  res.status(200).send();
 });
 
 app.listen(port, () => {
